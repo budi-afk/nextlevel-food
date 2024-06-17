f5112d18501f0792297fb810520b6a779c9dd7da
budi-afk <budiman1347@gmail.com>
2024-06-18 00:31:24 +0700

    outputing meals data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,28 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealGrid from "@/components/meals/meal.grid";
+
 export default function Page() {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meal, created{" "}
+          <span className={classes.highlight}>By You</span>{" "}
+        </h1>
+
+        <p>
+          Choose your favorite receipe and cook it yourself. It is easy and fun!
+        </p>
+
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite receipe</Link>
+        </p>
+      </header>
+
+      <main className="">
+        <MealGrid meals={[]} />
+      </main>
+    </>
+  );
 }
